068cc8eef13a6bf33ac37cf40bb5e73e5810a3a7
Gabrielesuma <gabrielesuma139@gmail.com>
2024-06-10 18:39:12 +0200

    chiamata api

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <h1 class="text-center">Ciao</h1>
   <i class="fa fa-solid fa-home"></i>
+  <ul>
+    <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+  </ul>
 </template>
 
 <script>
+import {store} from './store';
+import axios from 'axios';
   export default {
-    name: 'App'
+    name: 'App',
+    data(){
+      return{
+        store,
+        projects: []
+      }
+    },
+    methods: {
+      getAllProjects(){
+        axios.get(this.store.apiBaseUrl + '/projects').then((res) =>{
+          console.log(res.data);
+          this.projects = res.data.results;
+        });
+      }
+    },
+    mounted(){
+      this.getAllProjects();
+    }
   }
 </script>
 
